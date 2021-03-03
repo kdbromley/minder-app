1b18d543c434aa7eb12623194c7d4860138e5246
kd Bromley <kd.bromley1@gmail.com>
2021-03-02 17:01:28 -0800

    create dummy-store

---
+++ src/dummy-store.js
@@ -0,0 +1,85 @@
+export const STORE = { 
+    "reminders": [
+        {
+            "id": "1",
+            "title": 'Water Plants',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "Fern gets 3 cups, spritz flowers",
+            "alert": "email@host.com",
+            "checked": "false"
+        },
+        {
+            "id": "2",
+            "title": 'Change fishtank filter',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "lorem ipsum sic dolor",
+            "alert": "email@host.com",
+            "checked": "true"
+        },
+        {
+            "id": "3",
+            "title": 'Call back recruiter',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "",
+            "alert": "email@host.com",
+            "checked": "false"
+        },
+        {
+            "id": "4",
+            "title": 'Reminder 1',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "",
+            "alert": "email@host.com",
+            "checked": "false"
+        },
+        {
+            "id": "5",
+            "title": 'Reminder 2',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "lorem ipsum sic dolor",
+            "alert": "email@host.com",
+            "checked": "false"
+        },
+        {
+            "id": "6",
+            "title": 'Reminder 3',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "lorem ipsum sic dolor",
+            "alert": "email@host.com",
+            "checked": "true"
+        },
+        {
+            "id": "7",
+            "title": 'Reminder 4',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "",
+            "alert": "email@host.com",
+            "checked": "false"
+        },
+        {
+            "id": "8",
+            "title": 'Reminder 5',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "lorem ipsum sic dolor",
+            "alert": "email@host.com",
+            "checked": "false"
+        },
+        {
+            "id": "9",
+            "title": 'Reminder 6',
+            "dueDate": "2021-01-03",
+            "dueTime": "T00:00:00.000Z",
+            "notes": "",
+            "alert": "email@host.com",
+            "checked": "false"
+        },
+    ]
+}
