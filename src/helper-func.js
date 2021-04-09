5790e480d35b8b8f6e12f9b779acdb13e0e3316e
kd Bromley <kd.bromley1@gmail.com>
2021-04-08 19:32:46 -0700

    bug fix, type difference in === statment

--- src/helper-func.js
+++ src/helper-func.js
@@ -18,5 +18,6 @@ export const convertToReadable = (date) => {
 export let hourArray = ['12:00', '12:30', '1:00', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30']
 
 export const findReminder = (reminders, reminderId) => {
-    return reminders.find(reminder => reminder.id === reminderId)
+    const idNum = parseInt(reminderId)
+    return reminders.find(reminder => reminder.id === idNum)
 }
