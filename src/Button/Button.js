a755e21a798ffbb8e9c27da48d65f95e84197d6f
kd Bromley <kd.bromley1@gmail.com>
2021-04-15 13:38:12 -0700

    edit button icon added

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,6 +1,7 @@
 import deleteIcon from '../images/trash-2.svg'
 import checkCircleIcon from '../images/check-circle.svg';
 import checkIcon from '../images/check.svg';
+import editIcon from '../images/edit.svg';
 import './Button.css';
 
 export default function Button(props) {
@@ -10,11 +11,13 @@ export default function Button(props) {
       case 'Delete': return <img src={deleteIcon} alt='Delete Icon' />;
       case 'Check': return <img src={checkCircleIcon} alt='Check Icon' />;
       case 'Uncheck': return <img src={checkIcon} alt='Uncheck Icon' />;
+      case 'Edit': return <img src={editIcon} alt='Edit Icon' />;
+      default: return `${props.label}`;
     }
   }
 
   return (
-    <button className="Reminder__button" onClick={props.handleClick}>
+    <button className="Reminder__button" aria-label={`${props.label}`} onClick={props.handleClick}>
       {renderIcon()}
     </button>
   );
