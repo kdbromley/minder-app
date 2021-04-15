b0846da33dfa5c3af6771c229a318bbc36ccb09a
kd Bromley <kd.bromley1@gmail.com>
2021-04-14 17:18:35 -0700

    landing page styling, tweaks to main element sizing

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,13 +4,19 @@ import './LandingPage.css';
 
 export default function LandingPage(props) {
     return (
-        <div className='LandingPage'>
-            <h2>What's due today?</h2>
-              {props.reminders.map(reminder =>
-                <ReminderCard reminder={reminder} key={reminder.id}/>
-              )}
-            <Link to='/reminders' className='LandingPage__link'>See My Reminders &#10142;</Link>
-        </div>
+      <div className='LandingPage__container'>
+        <h2>What's due today?</h2>
+          {props.reminders.length !== 0
+          ? props.reminders.map(reminder =>
+            <ReminderCard reminder={reminder} key={reminder.id}/>
+          )
+          : <div className='LandingPage__wrapper'>
+              <h3 className='LandingPage__notif'>Nothing!</h3>
+              <span aria-hidden='true' className='unicode__check'>&#10003;</span>
+            </div>
+          }
+        <Link to='/reminders' aria-label='All My Reminders' className='LandingPage__all-link'>All My Reminders &#10142;</Link>
+      </div>
     )
 }
 
