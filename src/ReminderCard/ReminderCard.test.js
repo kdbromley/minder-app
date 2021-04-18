1e63091aa504f4ae63afacbf2414f919060172c4
kd Bromley <kd.bromley1@gmail.com>
2021-04-17 19:59:36 -0700

    fixed failing smoke test

--- src/ReminderCard/ReminderCard.test.js
+++ src/ReminderCard/ReminderCard.test.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import ReminderCard from './ReminderCard';
 
+const reminder = {
+  id: 1,
+  title: 'Test',
+  due_date: '2021-04-10T12:00:00.000Z',
+  reminder_notes: 'Test notes',
+  completed: false,
+  user_id: 1
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
   <BrowserRouter>
-    <ReminderCard />
+    <ReminderCard reminder={reminder}/>
   </BrowserRouter>, 
   div);
   ReactDOM.unmountComponentAtNode(div);
