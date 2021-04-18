127f17f572e3def2e53fd8a853b6b07790eafb95
kd Bromley <kd.bromley1@gmail.com>
2021-04-17 19:46:56 -0700

    fixed typo breaking cancel button

--- src/EditReminder/EditReminder.js
+++ src/EditReminder/EditReminder.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import { useState } from 'react';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
-import { convertToISO, formatAMPMForForm, formatDateForForm, formatDateForInput, formatTimeForForm, hourArray } from '../helper-func';
+import { convertToISO, formatAMPMForForm, formatDateForForm, formatTimeForForm, hourArray } from '../helper-func';
 import config from '../config';
 import './EditReminder.css';
 
@@ -13,15 +13,15 @@ export default function EditReminder(props) {
   
   const handleSubmit = e => {
     e.preventDefault();
-    const { title, date, hour, ampm, notes } = e.target;
-    const dueDate = convertToISO(date.value, hour.value, ampm.value)
+    const { title, due_date, hour, ampm, notes } = e.target;
+    const dueDate = convertToISO(due_date.value, hour.value, ampm.value)
     const updatedReminder = {
       'id': reminder.id,
       'title': title.value,
       'due_date': dueDate,
       'reminder_notes': notes.value,
       'completed': false,             
-      //no user_id field for editing, single dummyuser
+      //no user_id field for editing
     } 
     if(!updatedReminder.title || !updatedReminder.due_date) {
         //setState needs implementation for error message
@@ -98,7 +98,7 @@ export default function EditReminder(props) {
         label='Confirm Edit' />
         <Button className='EditReminder__button cancel' 
         label='Cancel and Go Back'
-         onClick={handleClickCancel} />
+        handleClick={handleClickCancel} />
       </div>
     </form>
   )
